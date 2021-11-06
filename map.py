f0cef79e356f615e721c71fe41d98c631cbbcab2
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-11-06 16:31:47 -0400

    Added callback for map

--- map.py
+++ map.py
@@ -66,30 +66,6 @@ us_state_to_abbrev = {
 }
 
 df = pd.read_csv('Veeva_Prescriber_Data.csv')
-df['Code'] = df['State'].map(us_state_to_abbrev)
-
-gb = df.groupby(['Code']).sum()
-
-dataForHover = df.groupby(['Code', 'Product']).sum().reset_index().astype(str)
-
-dataForHover['text'] = dataForHover['Product'] + ': ' + dataForHover['NRx_Month_1'] + '<br>'
-dataForHover = dataForHover.groupby(['Code']).sum()
-
-## Figure 1 is the USA map
-fig = go.Figure(data=go.Choropleth(
-    locations=gb.index, # Spatial coordinates
-    z = gb['NRx_Month_1'].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Reds',
-    colorbar_title = "Prescription Count",
-    text=dataForHover['text'], # hover text
-    marker_line_color='white', # line markers between states
-))
-
-fig.update_layout(
-    title_text = 'Veeva Data',
-    geo_scope='usa', # limit map scope to USA
-)
 
 ##Figure 2 is the error bar chart
 fig2 = go.Figure(data=go.Scatter(
@@ -102,21 +78,16 @@ fig2 = go.Figure(data=go.Scatter(
             valueminus=25)
     ))
 
-fig.update_layout(
-    title_text = 'Veeva Data Graph',
-)
-
 app = dash.Dash()
 app.layout = html.Div([
-    dcc.Graph(figure=fig,
-    id='graph-with-slider'),
+    dcc.Graph(id='graph-with-slider'),
 
     dcc.Slider(
         id = 'month-slider',
         min=1,
         max=6,
         marks={i: 'Month {}'.format(i) for i in range(1, 7)},
-        value=-3
+        value=1
     ),
     html.Div(id='my-output'),
 
@@ -138,12 +109,35 @@ app.layout = html.Div([
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    Input('month-slider', 'value'))
+    Input('month-slider', 'value')
+)
+def update_figure(month):
+    month_key = f'NRx_Month_{month}'
+    df['Code'] = df['State'].map(us_state_to_abbrev)
 
-def update_figure(selected_year):
-    #filtered_df = df[df.year == selected_year]
+    gb = df.groupby(['Code']).sum()
 
-    fig.update_layout(transition_duration=500)
+    dataForHover = df.groupby(['Code', 'Product']).sum().reset_index().astype(str)
+
+    dataForHover['text'] = dataForHover['Product'] + ': ' + dataForHover[month_key] + '<br>'
+    dataForHover = dataForHover.groupby(['Code']).sum()
+
+    ## Figure 1 is the USA map
+    fig = go.Figure(data=go.Choropleth(
+        locations=gb.index, # Spatial coordinates
+        z = gb[month_key].astype(float), # Data to be color-coded
+        locationmode = 'USA-states', # set of locations match entries in `locations`
+        colorscale = 'Reds',
+        colorbar_title = "Prescription Count",
+        text=dataForHover['text'], # hover text
+        marker_line_color='white', # line markers between states
+    ))
+
+    fig.update_layout(
+        title_text = 'Veeva Data',
+        geo_scope='usa', # limit map scope to USA
+        transition_duration=500
+    )
 
     return fig
 
